f4c572a1a7cc7a077bbb0141df91e24e45c38d49
Misachka <136197530+Misachka@users.noreply.github.com>
2023-09-23 14:15:59 -0400

    declared variables, prompted questions for readme

--- Develope/index.js
+++ Develope/index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+// Declaring variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generatemarkdown")
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+//Questions to generate the README.md
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is your project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "If any, Please escribe the installation process: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What will be your project used for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla Public License 2.0",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do to do if user has questions/issues? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
+  
+  init();  
